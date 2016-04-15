8036b9df67d4932e68efc15c7351a6a377615130
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-04-15 15:36:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,162 @@
+// smoothie-happy alias
+var sh = smoothieHappy;
+
+// load scanner local settings
+sh.network.scanner.store();
+
+//------------------------------------------------------------------------------
+
+// UI elements
+var $ipInput           = $('#ipInput');
+var $timeoutInput      = $('#timeoutInput');
+var $stopButton        = $('#stopButton');
+var $scanButton        = $('#scanButton');
+var $scanButtonIcon    = $scanButton.find('.icon');
+var $scanButtonText    = $scanButton.find('.text');
+var $scannedLabel      = $('#scannedLabel');
+var $totalLabel        = $('#totalLabel');
+var $foundLabel        = $('#foundLabel');
+
+//------------------------------------------------------------------------------
+
+// toggle scan button state
+function setScanButtonMode(mode) {
+    $scanButtonText.text(mode);
+
+    if (mode === 'scan' || mode === 'resume') {
+        if (mode === 'scan') {
+            $stopButton.toggleClass('hidden', true);
+        }
+        $scanButton.toggleClass('btn-primary', true);
+        $scanButton.toggleClass('btn-warning', false);
+        $scanButtonIcon.toggleClass('glyphicon-play', true);
+        $scanButtonIcon.toggleClass('glyphicon-pause', false);
+    }
+    else if (mode === 'pause') {
+        $stopButton.toggleClass('hidden', false);
+        $scanButton.toggleClass('btn-warning', true);
+        $scanButton.toggleClass('btn-primary', false);
+        $scanButtonIcon.toggleClass('glyphicon-pause', true);
+        $scanButtonIcon.toggleClass('glyphicon-play', false);
+    }
+}
+
+// update progression labels
+function setProgression(scanned, total, found) {
+    $scannedLabel.text(scanned);
+    $totalLabel.text(total);
+    $foundLabel.text(found);
+}
+
+//------------------------------------------------------------------------------
+
+// on scan button click
+$scanButton.on('click', function() {
+    // start scann
+    if (! sh.network.scanner.scanning) {
+        // if scan is aborted
+        if (sh.network.scanner.aborted) {
+            // resume scan
+            sh.network.scanner.resume($timeoutInput.val());
+        }
+        else {
+            // start scan
+            sh.network.scanner.scan($ipInput.val(), $timeoutInput.val());
+        }
+    }
+    else {
+        // abort scanning
+        sh.network.scanner.abort();
+    }
+    return false;
+});
+
+// on stop button click
+$stopButton.on('click', function() {
+    // stop scan
+    sh.network.scanner.stop();
+});
+
+//------------------------------------------------------------------------------
+
+// defaults inputs values
+$ipInput.val(sh.network.scanner.input);
+$timeoutInput.val(sh.network.scanner.timeout);
+
+// // on ip input change
+// $ipInput.on('change', function() {
+//     sh.network.scanner.setInput($ipInput.val());
+// });
+
+//
+// // on timeout input change
+// $timeoutInput.on('change', function() {
+//     sh.network.scanner.setTimeout($timeoutInput.val());
+// });
+
+//------------------------------------------------------------------------------
+
+// on scan start
+sh.network.scanner.onStart = function(queue) {
+    // reset progression
+    setProgression(0, 0, 0);
+
+    // set scan button mode
+    setScanButtonMode('pause');
+};
+
+// on scan progress (called after an ip was scanned)
+sh.network.scanner.onProgress = function(ip, board, scanner) {
+    // update progression
+    setProgression(scanner.scanned, scanner.total, scanner.found);
+
+    // set found label color
+    if (scanner.found) {
+        console.log('pouet');
+        $foundLabel.toggleClass('label-success', true);
+        $foundLabel.toggleClass('label-danger', false);
+    }
+    else {
+        $foundLabel.toggleClass('label-danger', true);
+        $foundLabel.toggleClass('label-success', false);
+    }
+
+    // board found
+    // if (board) {
+    //     console.log('smoothie:', ip, board);
+    // }
+};
+
+// on board found
+sh.network.scanner.onBoard = function(ip, board) {
+    console.log('smoothie:', ip, board);
+};
+
+// on scan aborted
+sh.network.scanner.onAbort = function(scanner) {
+    // set scan button mode
+    setScanButtonMode('resume');
+};
+
+// on resume scan
+sh.network.scanner.onResume = function(scanner) {
+    // set scan button mode
+    setScanButtonMode('pause');
+};
+
+// on scan stoped
+sh.network.scanner.onStop = function(scanner) {
+    // set scan button mode
+    setScanButtonMode('scan');
+};
+
+// on scan end
+sh.network.scanner.onEnd = function(found) {
+    // set scan button mode
+    setScanButtonMode('scan');
+};
+
+//------------------------------------------------------------------------------
+
+// debug...
+console.log('sh:', sh);
---
+++ src/smoothie-happy.js
@@ -0,0 +1,265 @@
++function (global) {
+
+    // global namespace
+    var sh = {
+        version    : '0.0.1-alpha',
+        name       : 'Smoothie Happy',
+        description: 'Smoothieware network communication API.'
+    };
+
+    // initialization message
+    console.info(sh.name + ' - v' + sh.version);
+
+    // -------------------------------------------------------------------------
+
+    // network namespace
+    sh.network = {
+        version: '0.0.1-alpha',
+        timeout: 1000
+    };
+
+    sh.network.post = function(uri, data, callback, timeout) {
+        var data = data || '';
+        var xhr  = new XMLHttpRequest();
+
+        xhr.timeout = timeout || this.timeout;
+
+        xhr.addEventListener('load', function() {
+            callback && callback('load', xhr);
+        });
+        xhr.addEventListener('error', function() {
+            callback && callback('error', xhr);
+        });
+        xhr.addEventListener('timeout', function() {
+            callback && callback('timeout', xhr);
+        });
+
+        //uri += '?time=' + Date.now(); // no-cache
+
+        xhr.open('POST', uri, true);
+        xhr.send(data);
+    };
+
+    // -------------------------------------------------------------------------
+
+    // network scanner
+    sh.network.scanner = {
+        version   : '0.0.1-alpha',
+        timeout   : 500,
+        scanning  : false,
+        scanned   : 0,
+        total     : 0,
+        found     : 0,
+        queue     : [],
+        boards    : {},
+        aborted   : false,
+        storage   : true,
+        input     : '192.168.1.*',
+        onStart   : function(queue) {},
+        onBoard   : function(ip, board) {},
+        onProgress: function(ip, board, self) {},
+        onAbort   : function(self) {},
+        onResume  : function(self) {},
+        onStop    : function(self) {},
+        onEnd     : function(found) {}
+    };
+
+    // scan an IP looking for a SmoothieBoard
+    sh.network.scanner.processQueue = function() {
+        if (!this.scanning) {
+            return false;
+        }
+
+        var ip = this.queue.shift();
+
+        if (! ip) {
+            this.onEnd(this.found);
+            this.scanning = false;
+            return true;
+        }
+
+        var self  = this;
+        var board = null;
+        var uri   = 'http://' + ip + '/command';
+
+        console.log('scan:', ip);
+
+        sh.network.post(uri, 'version\n', function(type, xhr) {
+
+            if (type === 'load' && xhr.status === 200) {
+                var text = xhr.responseText;
+
+                // expected : Build version: edge-94de12c, Build date: Oct 28 2014 13:24:47, MCU: LPC1769, System Clock: 120MHz
+                var matches = text.match(/Build version: (.*), Build date: (.*), MCU: (.*), System Clock: (.*)/);
+
+                if (matches) {
+                    board = {
+                        version: matches[1],
+                        date   : matches[2],
+                        mcu    : matches[3],
+                        clock  : matches[4]
+                    };
+                    self.found++;
+                    self.boards[ip] = board;
+                    self.onBoard(ip, self.boards[ip]);
+                }
+            }
+
+            self.scanned++;
+            self.onProgress(ip, board, self);
+            self.processQueue();
+
+        }, this.timeout);
+    };
+
+    // locale storage shortcut
+    sh.network.scanner.store = function(index, value) {
+        // no locale storage
+        if (!this.storage) return;
+
+        // get the store
+        var store = JSON.parse(localStorage.getItem('sh.network.scanner') || '{}');
+
+        // load stored values
+        if (! index) {
+            for (var index in store) {
+                this[index] = store[index];
+            }
+            return;
+        }
+
+        // get old value at index
+        var oldValue = store[index] || undefined;
+
+        // store the new value
+        if (arguments.length > 1) {
+            store[index] = value;
+            localStorage.setItem('sh.network.scanner', JSON.stringify(store));
+        }
+
+        // return the old value
+        return oldValue;
+    };
+
+    // set timeout
+    sh.network.scanner.setTimeout = function(timeout) {
+        if (timeout < 100 || timeout > 2000) {
+            throw new Error('Timeout is out of range [100, 2000].');
+        }
+        this.timeout = timeout;
+        this.store('timeout', timeout);
+    };
+
+    // set the input and compute the scan queue
+    sh.network.scanner.setInput = function(input) {
+        // reset queue
+        this.queue = [];
+
+        // To short or not defined
+        if (!input || input.length < 3) {
+            throw new Error('Invalid input.');
+        }
+
+        // Wildcard | ex.: [192.168.1.*]
+        else if (/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.\*$/.test(input)) {
+            var inputParts = input.split('.');
+            inputParts.pop(); // remove last part (*)
+            var baseIp = inputParts.join('.');
+            for (var i = 0; i <= 255; i++) {
+                this.queue.push(baseIp + '.' + i);
+            }
+        }
+
+        // Single ip | ex.: [192.168.1.55]
+        else if (/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/.test(input)) {
+            this.queue.push(input);
+        }
+
+        // Ip's range | ex.: [192.168.1.50-100]
+        else if (/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\-[0-9]{1,3}$/.test(input)) {
+            var inputParts = input.split('.');
+            var inputRange = inputParts.pop().split('-'); // last part (xxx-xxx)
+            var baseIp     = inputParts.join('.');
+            for (var i = inputRange[0]; i <= inputRange[1]; i++) {
+                this.queue.push(baseIp + '.' + i);
+            }
+        }
+
+        // Hostname | ex.: [www.host.name]
+        else if (/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(input)) {
+            this.queue.push(input);
+        }
+
+        // Invalid...
+        else {
+            throw new Error('Invalid input.');
+        }
+
+        // set input
+        this.input = input;
+        this.store('input', input);
+
+        // return the queue
+        return this.queue;
+    };
+
+    // scan the network looking for a SmoothieBoard
+    sh.network.scanner.scan = function(input, timeout) {
+        if (this.scanning) {
+            throw new Error('Already in scan mode.');
+        }
+
+        input && this.setInput(input);
+        timeout && this.setTimeout(timeout);
+
+        this.scanning = true;
+        this.aborted  = false;
+        this.total    = this.queue.length;
+        this.scanned  = 0;
+        this.boards   = {};
+        this.found    = 0;
+
+        this.onStart(this.queue);
+        this.processQueue();
+    };
+
+    // stop scanning
+    sh.network.scanner.stop = function(silent) {
+        if (this.scanning || this.aborted) {
+            !silent && this.onStop(this);
+            this.scanning = false;
+            this.aborted  = false;
+            return true;
+        }
+        return false;
+    };
+
+    // abort scanning
+    sh.network.scanner.abort = function() {
+        if (this.stop(true)) {
+            this.aborted = true;
+            this.onAbort(this);
+            return true;
+        }
+        return false;
+    };
+
+    // resume aborted scanning
+    sh.network.scanner.resume = function(timeout) {
+        if (this.aborted) {
+            timeout && this.setTimeout(timeout);
+            this.scanning = true;
+            this.aborted = false;
+            this.onResume(this);
+            this.processQueue();
+            return true;
+        }
+        return false;
+    };
+
+    // -------------------------------------------------------------------------
+
+    // export global namespace
+    global.smoothieHappy = sh;
+
+}(window);
