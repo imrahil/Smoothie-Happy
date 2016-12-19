1ee40de7d3bcf7f42afd8b54425d388e6291b0d0
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-12-19 18:02:04 +0100

    Add Request example

--- src/request.js
+++ src/request.js
@@ -12,6 +12,36 @@ import { RequestEvent, ProgressEvent } from './events';
 * @param {Integer} [settings.timeout = 5000]  Timeout for this request in milliseconds.
 * @param {Object}  [settings.xhr     = null]  An `XMLHttpRequest` instance or an collection of `XMLHttpRequest` properties/methods to overwrite.
 * @see Please read {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise|this} and {@link https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html|that} to learn more about promises.
+* @example
+* // Single request
+* new Request({
+*     url: 'hello.html'
+* })
+* .onProgress(function(event) {
+*     // notify progression
+*     console.info('on:progress', event);
+* })
+* .then(function(event) {
+*     // hello.html is loaded
+*     console.info('on:load', event);
+*
+*     // return result for final then
+*     return { event: event, error: false };
+* })
+* .catch(function(event) {
+*     // error loading hello.html
+*     console.warn('on:error', event);
+*
+*     // return error for final then
+*     return { event: event, error: true };
+* })
+* .then(function(result) {
+*     // finally ...
+*     var event = result.event;
+*     var type  = result.error ? 'error' : 'info';
+*
+*     console[type]('finally:', event);
+* });
 */
 class Request {
     constructor(settings) {
